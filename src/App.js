182bc5f00a94b6a97825bd8eb506d9d13cd32871
PavithranBaskaran <vktpavi@gmail.com>
2022-07-06 20:28:59 +0530

    in progress

--- src/App.js
+++ src/App.js
@@ -78,12 +78,14 @@ function App() {
          <div className=" d-flex justify-content-center">
         <hr className=" border opacity-50 mt-5 line "/>
         </div>
-        <section className="" id="project">Project</section>
+        <section className="" id="project">Project
+        <h1>IN PROGRESS</h1></section>
         {/* Horizontal Line */}
         <div className=" d-flex justify-content-center">
         <hr className=" border opacity-50 mt-5 line "/>
         </div>
-        <section className="" id="contact">Contact</section>
+        <section className="" id="contact">Contact
+        <h1>IN PROGRESS</h1></section>
         {/* Horizontal Line */}
         <div className=" d-flex justify-content-center">
         <hr className=" border opacity-50 mt-5 line "/>
