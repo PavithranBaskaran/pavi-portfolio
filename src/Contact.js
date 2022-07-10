c3e8216a3bc711f282a817401033c7f35fba8959
PavithranBaskaran <vktpavi@gmail.com>
2022-07-10 09:23:31 +0530

    updated cell call

--- src/Contact.js
+++ src/Contact.js
@@ -13,63 +13,63 @@ function Contact() {
     <>
       <section className="m-lg-5 " id="contact">
         <h1 className="fingerpaint about-head">Contact me</h1>
-        <div class="container-fluid px-4 mt-5">
-          <div class="row gx-5 gy-4">
-            <div data-aos="fade-left" class="col">
+        <div data-aos="fade-right" class="container-fluid px-4 mt-5">
+          <div class="row pb-5  gx-5 gy-4">
+            <div  class="col">
               <div 
                 style={{ backgroundColor: "#c11625" }}
-                class="contact-pill p-2 border rounded-pill d-flex align-center justify-center  border-danger"
+                className="contact-pill p-2 border rounded-pill d-flex align-center   border-danger"
                 onClick={() =>
                   window.open("mailto:vktpavi@gmail.com", "_blank")
                 }
               >
                 <AiOutlineMail
                   color="#c71610"
-                  size={80}
-                  className="me-2 border inside  rounded-circle p-2 d-flex justify-content-start"
+                 
+                  className="me-2 border inside  rounded-circle hint-icon p-2 d-flex justify-content-start"
                 />
                 <div className="d-flex align-items-center">
-                  <p className="fs-4 m-0">vktpavi@gmail.com</p>
+                  <p className="hint m-0">vktpavi@gmail.com</p>
                 </div>
               </div>
             </div>
-            <div data-aos="fade-right" class="col">
+            <div  className="col">
               <div 
                 style={{ backgroundColor: "#128C7E" }}
-                class="contact-pill p-2 border rounded-pill d-flex align-items-center justify-center border-success"
-                onClick={() => window.open("callto://+918778702987", "_blank")}
+                className="contact-pill p-2 border rounded-pill d-flex align-items-center justify-center border-success"
+                onClick={() => window.open("tel://+918778702987", "_blank")}
               >
                 <FcPhoneAndroid
-                  size={80}
-                  className="me-5 border inside rounded-circle p-2 d-flex justify-content-start"
+                  
+                  className="me-5 border inside hint-icon rounded-circle p-2 d-flex justify-content-start"
                 />
                 <div className="d-flex align-items-center">
-                  <p className="fs-4 m-0">8778702987</p>
+                  <p className=" hint m-0">8778702987</p>
                 </div>
               </div>
             </div>
-            <div data-aos="fade-left" class="col">
+            <div  className="col">
               <div
                 style={{ backgroundColor: "#128C7E" }}
-                class="contact-pill p-2 border rounded-pill d-flex align-items-center justify-center border-success"
+                className="contact-pill p-2 border rounded-pill d-flex align-items-center justify-center border-success"
                 onClick={() =>
                   window.open("https://wa.me/+918778702987", "_blank")
                 }
               >
                 <ImWhatsapp
                   color="#25D366"
-                  size={80}
-                  className="me-5 border inside rounded-circle p-2 d-flex justify-content-start"
+                  
+                  className="me-5 border inside hint-icon rounded-circle p-2 d-flex justify-content-start"
                 />
                 <div className="d-flex align-items-center">
-                  <p className="fs-4 m-0">8778702987</p>
+                  <p className="hint m-0">8778702987</p>
                 </div>
               </div>
             </div>
-            <div data-aos="fade-right" class="col">
+            <div  className="col">
               <div
                 style={{ backgroundColor: "#0a66c2" }}
-                class="contact-pill p-2 border rounded-pill d-flex align-items-center justify-center border-primary "
+                className="contact-pill p-2 border rounded-pill d-flex align-items-center justify-center border-primary "
                 onClick={() =>
                   window.open(
                     "https://www.linkedin.com/in/pavithran-b-8026071a3/",
@@ -79,11 +79,11 @@ function Contact() {
               >
                 <FaLinkedin
                   color="#0a66c2"
-                  size={80}
-                  className="me-5 border inside rounded-circle p-2 d-flex justify-content-start"
+                  
+                  className="me-5 border inside hint-icon rounded-circle p-2 d-flex justify-content-start"
                 />
                 <div className="d-flex align-items-center">
-                  <p className="fs-3 m-0">Linkedin</p>
+                  <p className="hint m-0">Linkedin</p>
                   {/* <p className="fs-4 m-0">vktpavi@gmail</p> */}
                 </div>
               </div>
